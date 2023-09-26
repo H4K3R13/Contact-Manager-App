812532ecdc17560c958fdb9d015bc7de3475b756
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-09-26 23:20:54 +0530

    🥂Fixed the middleware

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -13,7 +13,13 @@ const validateToken = asyncHandler(async (req, res, next) => {
         throw new Error("User not Authorized");
       }
       console.log(decoded);
+      req.user = decoded.user;
+      next();
     });
+    if(!token){
+      res.status("401");
+      throw new Error("User is not authorized or token is missing")
+    }
   }
 });
 
